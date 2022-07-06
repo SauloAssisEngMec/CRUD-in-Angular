7aec791893b79b74c4959a67ec74fe8682033c36
Saulo <sauloassis4123@gmail.com>
2022-07-06 18:01:01 -0300

    with db json

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -11,17 +12,17 @@ export class ApiService {
 
 
   postProduct(data : any){
-    return this.http.post<any>("http://localhost:3000/productList/",data);
+    return this.http.post<any>(environment.URL,data);
   }
   getProduct(){
-    return this.http.get<any>("http://localhost:3000/productList/");
+    return this.http.get<any>(environment.URL);
   }
   putProduct(data:any, id : number){
-    return this.http.put<any>("http://localhost:3000/productList/" + id, data);
+    return this.http.put<any>(environment.URL + id, data);
   } 
 
   deleteProduct(id: number){
-    return this.http.delete<any>("http://localhost:3000/productList/"+id);
+    return this.http.delete<any>(environment.URL+id);
   }
 
 
