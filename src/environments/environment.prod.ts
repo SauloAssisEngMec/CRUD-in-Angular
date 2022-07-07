531d0789de9330537f8399ffe645432e0929f4a9
Saulo <sauloassis4123@gmail.com>
2022-07-07 13:52:11 -0300

    solve vercel problem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL: 'https://my-json-server.typicode.com/SauloAssisEngMec/db-jsonForCrud/productList'
+  URL: 'https://my-json-server.typicode.com/SauloAssisEngMec/db-jsonForCrud/productList/'
 };
